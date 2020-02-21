5d36407998865a6ce3f39c73af93b1d7fb9b0959
jasonlbx13 <602465307@qq.com>
2020-02-21 15:57:59 +0800

    update req&so

--- source/setup.py
+++ source/setup.py
@@ -2,21 +2,27 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(
-    ext_modules = cythonize("aimodel_kw.py")
-)
-
-setup(
-    ext_modules = cythonize("aimodel_fnlp.py")
-)
-
 # setup(
-#     ext_modules = cythonize("hmai/hmai_gate_nlp.pyx")
+#     ext_modules = cythonize("aimodel_kw.py")
+# )
+#
+# setup(
+#     ext_modules = cythonize("aimodel_fnlp.py")
+# )
+#
+# setup(
+#     ext_modules = cythonize("aimodel_cherry.py")
+# )
+#
+# setup(
+#     ext_modules = cythonize("hmai/hmai_gate_nlp.py")
 # )
 # setup(
-#     ext_modules = cythonize("hmai/hmai_group_nlp.pyx")
+#     ext_modules = cythonize("hmai/hmai_group_nlp.py")
 # )
-
+setup(
+    ext_modules = cythonize("hmai/hmai_base_aimodel.py")
+)
 
 
 # python setup.py build_ext --inplace
